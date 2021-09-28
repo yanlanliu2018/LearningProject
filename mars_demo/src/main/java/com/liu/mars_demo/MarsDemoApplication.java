dcebe75a81c4af88815a2e92c8c43d300585c1af
yanlan.liu <yanlan.liu@envision-digital.com>
2021-09-28 11:55:14 +0800

    新建 spring boot 项目模块，mars_demo。

---
+++ mars_demo/src/main/java/com/liu/mars_demo/MarsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.liu.mars_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarsDemoApplication.class, args);
+    }
+
+}
---
+++ mars_demo/src/test/java/com/liu/mars_demo/MarsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liu.mars_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
